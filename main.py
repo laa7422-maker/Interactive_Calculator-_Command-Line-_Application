cdd67149da9d152302b4956130938257b94e6107
laa7422-maker <laa7422@njit.edu>
2026-06-07 12:31:18 -0700

    Fix: enforce 100% coverage in pytest.ini, pragma no cover on main entry point

--- main.py
+++ main.py
@@ -13,7 +13,5 @@ from app.calculator import calculator
 
 # So, what this line means is: "If you're running this program directly (not as part of another program), 
 # then go ahead and start the calculator."
-if __name__ == "__main__":
-    # Now, we use the calculator tool we got earlier. This will start the calculator, which is a program 
-    # that keeps running and doing math based on what we tell it.
+if __name__ == "__main__":  # pragma: no cover
     calculator()
